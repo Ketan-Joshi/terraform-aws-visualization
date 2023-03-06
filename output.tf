aed5a3bca4b102c803dd8cfbb510a5e801abf7a7
Ketan Joshi <ketanjoshi@TTNs-MacBook-Pro.local>
2023-03-06 17:03:11 +0530

    Fixes

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "private_ip" {
   value = aws_instance.visulization.private_ip
 }
+output "visualization_pem_file" {
+  value = tls_private_key.ssh_private_key.private_key_pem
+  sensitive = true
+}
