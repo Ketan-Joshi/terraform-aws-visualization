3d6f640adaea5f338fc5d281bf7596bd38e3b1c1
Ketan Joshi <ketanjoshi@TTNs-MacBook-Pro.local>
2023-03-17 16:21:06 +0530

    Fixes

--- visualization.sh
+++ visualization.sh
@@ -16,7 +16,8 @@ sudo systemctl start amazon-ssm-agent
 ## for installing the CW-agent ##
 #################################
 cd /opt && wget https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm && rpm -U ./amazon-cloudwatch-agent.rpm
-echo '
+
+cat <<EOF >>/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent.json
 {
         "agent": {
                 "metrics_collection_interval": 60,
@@ -45,10 +46,13 @@ echo '
                                 "metrics_collection_interval": 60
                         }
                 },
-  "aggregation_dimensions" : [ ["InstanceId", "InstanceType"]]
+                "aggregation_dimensions" : [ 
+                        ["InstanceId", "InstanceType"]
+                ]
         }
 }
-'>/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent.json
+EOF
+
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent.json
 sudo systemctl restart amazon-cloudwatch-agent.service
 
